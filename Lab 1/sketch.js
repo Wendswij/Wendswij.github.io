f0acf8a6e1772ca4a80130c43f2c1a04f7c0bfef
Wendswij <wendywendy669@gmail.com>
2025-01-31 23:33:03 -0500

    Lab 1 Version

---
+++ Lab 1/sketch.js
@@ -0,0 +1,22 @@
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+    background(123, 104, 238);
+    fill('#90EE90');
+    stroke('#008080');
+    for(let i = 0; i < 10000; i++){
+        let x = (i*12) % width;
+        let y = (i*11) % height;
+        triangle(x, y, x+10, y+15, x-10, y+15);
+    }
+    
+    fill('#B0E0E6');
+    stroke('#0000CD');
+    if(mouseX < 700){
+        circle(mouseX,mouseY,80);
+    }else{
+        rect(mouseX,mouseY,60,40);
+    }
+}
